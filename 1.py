6c3953d5f39cc82e89ae7a54e9b25843108b6b78
vaanii2006-eng <vaanii2006@gmail.com>
2026-02-17 17:15:48 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Largest number is", a)
+elif b > a:
+    print("Largest number is", b)
+else:
+    print("Both numbers are equal")
